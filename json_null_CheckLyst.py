d8da2736db9260ea50204f1f0ab53b71268b8639
alexoleam <81093371+alexoleam@users.noreply.github.com>
2022-01-09 12:34:48 -0400

    Update json_null_CheckLyst.py
    Corrected indentation errors

--- json_null_CheckLyst.py
+++ json_null_CheckLyst.py
@@ -2,14 +2,14 @@ import json
 with open('/path/to/josn_file.json', 'r') as file:
      json_data = json.load(file)
      for item in json_data:
-	       if item['validity'] not in [""]:
+		if item['validity'] not in [""]:
 		      item['validity'] = ""
-	       if item['read only'] not in [""]:
+		if item['read only'] not in [""]:
 		      item['read only'] = ""
-	       if item['hidden'] not in [false]:
+		if item['hidden'] not in [false]:
 		      item['hidden'] = false
-           if item['code'] not in [""]:
-              item['code'] = ""
+		if item['code'] not in [""]:
+			item['code'] = ""
 
 with open('/path/to/josn_file.json', 'w') as file:
     json.dump(json_data, file, indent=2)
