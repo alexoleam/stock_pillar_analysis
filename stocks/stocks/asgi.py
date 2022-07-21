ffaf06653dc0a2be1bba7f38d9ccf2659cb6f542
Alexander Rosado <aortellado110@gmail.com>
2022-07-21 17:45:08 -0400

    Commit to update files to view settings-template.py file.

--- stocks/stocks/asgi.py
+++ stocks/stocks/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stocks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stocks.settings-template')
 
 application = get_asgi_application()
--- stocks/stocks/wsgi.py
+++ stocks/stocks/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stocks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stocks.settings-template')
 
 application = get_wsgi_application()
